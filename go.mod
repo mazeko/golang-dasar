d194a5d816f5172a88712f278a1828c5c187b52f
mazeko <Eko.Purnomo@sera.astra.co.id>
2022-10-09 17:18:02 +0700

    how to use module

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mazeko/golang-dasar
+
+go 1.19
+
+require github.com/mazeko/go_sayhello v0.0.0-20221009101028-887926be7653 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_sayHello "github.com/mazeko/go_sayhello"
+)
+
+func main() {
+	fmt.Println(go_sayHello.SayHello())
+}
