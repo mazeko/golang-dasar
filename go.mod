9c234fee2b389398fdb7abeb466e73021dd79dd9
mazeko <Eko.Purnomo@sera.astra.co.id>
2022-10-17 16:17:21 +0700

    upgrade dependency

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/mazeko/golang-dasar
 
 go 1.19
 
-require github.com/mazeko/go_sayhello v1.0.1
+require github.com/mazeko/go_sayhello/v2 v2.0.0 // indirect
--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	go_sayHello "github.com/mazeko/go_sayhello"
+	go_sayHello "github.com/mazeko/go_sayhello/v2"
 )
 
 func main() {
-	fmt.Println(go_sayHello.SayHello())
+	fmt.Println(go_sayHello.SayHello("Eko"))
 }
